d13085255cf54fce0eac1548d203c31a7cf1365d
datcode2021 <datcoin2021@gmail.com>
2022-08-30 16:29:47 +0700

    test 3

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required.
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0x54413C1Ab17392b3dC27aa62E2AaeA9D778b146e"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = '0x54413C1Ab17392b3dC27aa62E2AaeA9D778b146e'; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
